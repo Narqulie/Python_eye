0702b08c376445d151b92097e185e019a1f61f26
narqulie <Narqulie@gmail.com>
2023-08-29 08:37:38 +0300

    Initial commit ```

---
+++ main.py
@@ -0,0 +1,37 @@
+import logging
+
+logging.basicConfig(filename="app.log",
+                    level="INFO",
+                    filemode="w",
+                    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+
+def main():
+    while True:
+        logging.info("Starting face recognition program.")
+        print("Choose an option:")
+        print("1. Train face recognizer")
+        print("2. Start webcam face recognition")
+        print("3. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            logging.info("Training face recognizer...")
+            import train
+            train.run()  # Run the training script
+
+        elif choice == "2":
+            logging.info("Starting webcam face recognition...")
+            import webcam_recognition
+            webcam_recognition.run()  # Run the webcam face recognition script
+            
+        elif choice == "3":
+            logging.info("Exiting...")
+            break
+        else:
+            logging.warning("Invalid choice. Please try again.")
+
+
+if __name__ == "__main__":
+    main()
---
+++ train.py
@@ -0,0 +1,60 @@
+import cv2
+import os
+import numpy as np
+import logging
+import json
+
+# Initialize face detector
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Directory containing the dataset
+dataset_path = 'dataset/'
+
+# Lists to hold faces and labels
+faces = []
+labels = []
+label_counter = 0
+
+# Mapping of label numbers to person names, based on folder names
+label_map = {}
+
+
+# Iterate through dataset directories and images
+def run():
+    global label_counter
+    for person_name in os.listdir(dataset_path):
+        person_path = os.path.join(dataset_path, person_name)
+        
+        if os.path.isdir(person_path):
+            label = label_counter  # Assign the current label for the person
+            label_map[label] = person_name  # Map the label number to the person name
+            for image_name in os.listdir(person_path):
+                image_path = os.path.join(person_path, image_name)
+                image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)  # Convert to grayscale
+
+                # Detect faces in the image
+                detected_faces = face_cascade.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5)
+                
+                for (x, y, w, h) in detected_faces:
+                    face = image[y:y+h, x:x+w]
+                    faces.append(face)
+                    labels.append(label)
+            
+            label_counter += 1  # Increment the label counter after processing all images of the current person
+
+    logging.info(f"Found {len(faces)} faces from {len(set(labels))} persons.")
+    # Initialize the LBPH face recognizer
+    face_recognizer = cv2.face.LBPHFaceRecognizer_create()
+
+    # Train the recognizer
+    face_recognizer.train(faces, np.array(labels))
+    
+    # Save the trained model
+    model_path = "face_recognizer_model.yml"
+    face_recognizer.save(model_path)
+
+    # write out label_map to json file
+    str_label_map = {str(k): v for k, v in label_map.items()}
+    with open('label_map.json', 'w') as fp:
+        json.dump(str_label_map, fp)
+    logging.info(f"Saved label map: {str_label_map}")
